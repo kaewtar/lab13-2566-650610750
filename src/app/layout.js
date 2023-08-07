c56ee572f64385cf61714a5bc4bbe2020dedebf3
kaewtar <kaewtarcmu@gmail.com>
2023-08-07 15:50:48 +0700

    completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,11 +26,7 @@ export default function RootLayout({ children }) {
           <Header />
           <Navbar />
           {children}
-          <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
-          />
+          <Footer year="2023" fullName="Kaewtar Lungta" studentId="650610750" />
         </div>
       </body>
     </html>
--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,13 +3,13 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({ params }) {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
 
-  /*
-  tip2 : Use "includes" string method to check substring
+  /*tip2 : Use "includes" string method to check substring
   Example : "ABC".includes("AB") -> return true
+  const result = processedSearchInput.includes(MovieRow.name)
 
   tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
   to convert movie title and searchInput to lower case 
@@ -17,14 +17,35 @@ export default function SearchResultPage() {
     you code here...
   );
   */
+  const searchInput = params.searchInput;
+  const processedSearchInput = searchInput.replaceAll("%20", " ");
+
+  const filteredMoviesMovie = movieDB.filter((movie) =>
+    movie.title.toUpperCase().includes(processedSearchInput.toUpperCase())
+  );
+
+  if (!filteredMoviesMovie) {
+    return <p className="text-center"> Movie not found </p>;
+  }
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">
+        Found {filteredMoviesMovie.length} result(s)
+      </p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {filteredMoviesMovie.map((movie, i) => (
+        <MovieRow
+          id={movie.id}
+          title={movie.title}
+          detail={movie.detail}
+          rating={movie.rating}
+          number={i + 1}
+        />
+      ))}
     </div>
   );
 }
